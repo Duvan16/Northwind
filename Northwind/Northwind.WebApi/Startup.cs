7404f0d3075efca639c5b821cc34816a620c18dc
Duvan <duvan4448@gmail.com>
2020-06-12 00:48:07 -0500

    5-Repositorio-Clientes

--- Northwind/Northwind.WebApi/Startup.cs
+++ Northwind/Northwind.WebApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Northwind.DataAcccess;
+using Northwind.UnitOfWork;
 
 namespace Northwind.WebApi
 {
@@ -25,6 +27,9 @@ namespace Northwind.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IUnitOfWork>(option => new NorthwindUnitOfWork(
+                Configuration.GetConnectionString("Northwind")
+                ));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
