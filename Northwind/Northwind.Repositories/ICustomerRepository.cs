b7b5ae02f4fe981fe6a0f1f046a58ac66e70e049
Duvan <duvan4448@gmail.com>
2020-06-11 23:49:14 -0500

    4- Repositorio-Clientes

---
+++ Northwind/Northwind.Repositories/ICustomerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Northwind.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Repositories
+{
+    public interface ICustomerRepository : IRepository<Customer> 
+    {
+        IEnumerable<Customer> CustomerPagedList(int page, int rows);
+    }
+}
