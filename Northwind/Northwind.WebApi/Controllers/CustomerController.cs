4332acc4a146bbad48fa4d9927e488cf87e02b8e
Duvan <duvan4448@gmail.com>
2020-06-12 01:28:50 -0500

    6-Creacion-Controlador-Clientes

---
+++ Northwind/Northwind.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.VisualStudio.Web.CodeGeneration.Contracts.Messaging;
+using Northwind.Models;
+using Northwind.UnitOfWork;
+
+namespace Northwind.WebApi.Controllers
+{
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CustomerController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unitOfWork.Customer.GetById(id));
+        }
+
+        [HttpGet]
+        [Route("GetPaginatedCustomer/{page:int}/{rows:int}")]
+        public IActionResult GetPaginatedCustomer(int page,int rows)
+        {
+            return Ok(_unitOfWork.Customer.CustomerPagedList(page,rows));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Customer customer)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            return Ok(_unitOfWork.Customer.Insert(customer));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody]Customer customer)
+        {
+            if (ModelState.IsValid && _unitOfWork.Customer.Update(customer))
+            {
+                return Ok(new { Message = "The Customer is Updated" });
+            }
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete([FromBody]Customer customer)
+        {
+            if (customer.Id > 0)
+                return Ok(_unitOfWork.Customer.Delete(customer));
+            return BadRequest();
+        }
+    }
+}
